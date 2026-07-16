9b3b5897903decd2f127b14a0dc2e3b2dc2b9404
Dhruv <178195165+dhruvxsachdeva@users.noreply.github.com>
2026-07-16 16:06:10 +0530

    basic multi head

--- transformer/transformer.py
+++ transformer/transformer.py
@@ -82,7 +82,21 @@ class Head(nn.Module):
         v = self.value(x) # (B,T,hs)
         out = wei @ v # (B, T, T) @ (B, T, hs) -> (B, T, hs)
         return out
+
+
+class MultiHeadAttention(nn.Module):
+    """ multiple heads of self-attention in parallel """
+
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+    def forward(self, x):
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
+        
+        return out
     
+
+
 # super simple bigram model
 class BigramLanguageModel(nn.Module):
 
@@ -91,7 +105,7 @@ class BigramLanguageModel(nn.Module):
         # each token directly reads off the logits for the next token from a lookup table
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table=nn.Embedding(block_size,n_embd)# encoding postions of tokens
-        self.sa_head=Head(n_embd)
+        self.sa_heads=MultiHeadAttention(4,n_embd//4) # 4 heads of 8 dimensional self attention, it has to be integer division because nn.Linear requires out_features to be an integer, because tensor dimensions must be integers.
         self.lm_head= nn.Linear(n_embd,vocab_size)
 
     def forward(self, idx, targets=None):
@@ -100,7 +114,7 @@ class BigramLanguageModel(nn.Module):
         token_emb = self.token_embedding_table(idx) # (B,T,C)
         pos_emb=self.position_embedding_table(torch.arange(T,device=device))
         x=token_emb+pos_emb # x holds both token identities and positions that this tokens occur at.
-        x=self.sa_head(x) 
+        x=self.sa_heads(x) 
         logits=self.lm_head(x) # B,T, vocab_size
        
 
